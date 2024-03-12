5699e520702b58c3bf93c2ad8881eaf7bf169411
정가현 <135098077+gahyun01@users.noreply.github.com>
2024-03-13 04:54:39 +0900

    Add files via upload

---
+++ chapter2/2-1.py
@@ -0,0 +1,18 @@
+a = 1
+print(type(a))
+
+a = 1.24
+print(type(a))
+
+a = 4.24e10
+b = 4.24e-10
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+
+a = 0o37
+print(type(a))
+
+a = 0x7A
+print(type(a))
